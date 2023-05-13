020c5bc765a4d8f6bebe1b335546512a27758521
Rohit <theunderrated17@gmail.com>
2023-05-13 17:53:15 +0530

    latest update

--- js/main.js
+++ js/main.js
@@ -64,44 +64,45 @@ $(function () {
   /*********  Custom functions Area *********/
 
   function setThemeMode(themeColor) {
-    if (themeColor === darkTheme_class) {
-      pageBody.addClass(darkTheme_class);
-      modeSwitcher.addClass(darkTheme_class).removeClass(lightTheme_class);
-      localStorage.setItem(themeStoredKey, darkTheme_class);
-      localStorage.removeItem(lightTheme_class);
-    }
+    
     if (themeColor === lightTheme_class) {
       pageBody.removeClass(darkTheme_class);
       modeSwitcher.addClass(lightTheme_class).removeClass(darkTheme_class);
       localStorage.setItem(themeStoredKey, lightTheme_class);
       localStorage.removeItem(darkTheme_class);
     }
+    if (themeColor === darkTheme_class) {
+      pageBody.addClass(darkTheme_class);
+      modeSwitcher.addClass(darkTheme_class).removeClass(lightTheme_class);
+      localStorage.setItem(themeStoredKey, darkTheme_class);
+      localStorage.removeItem(lightTheme_class);
+    }
   }
 
   /* *******  Set the theme according to the local storage value ********/
   // if local storge not set or the body has class value of .dark-theme THEN default theme is dark
-  if (!themeStoredItem && !pageBody.hasClass(darkTheme_class)) {
-    setThemeMode(darkTheme_class);
+  if (!themeStoredItem && !pageBody.hasClass(lightTheme_class)) {
+    setThemeMode(lightTheme_class);
   }
   // the only case to be light mode is when the local storge has he value of light-theme
-  if (themeStoredItem === lightTheme_class) {
-    setThemeMode(lightTheme_class);
+  if (themeStoredItem === darkTheme_class) {
+    setThemeMode(darkTheme_class);
   }
 
   // if local storge or the body has class value of .dark-theme
   if (
-    themeStoredItem === darkTheme_class ||
-    pageBody.hasClass(darkTheme_class)
+    themeStoredItem === lightTheme_class ||
+    pageBody.hasClass(lightTheme_class)
   ) {
-    setThemeMode(darkTheme_class);
+    setThemeMode(lightTheme_class);
   }
 
   /* ******* Set the theme by clicking the theme switcher ********/
   $(modeSwitcher).on("click", function () {
-    if ($(this).is("." + darkTheme_class)) {
-      setThemeMode(lightTheme_class);
-    } else if ($(this).is("." + lightTheme_class)) {
+    if ($(this).is("." + lightTheme_class)) {
       setThemeMode(darkTheme_class);
+    } else if ($(this).is("." + darkTheme_class)) {
+      setThemeMode(lightTheme_class);
     }
   });
 
